95cc9a2b6d09808ee7a7af5791dac633f5819e65
PeterOlveraO <pedro.olvera.8a@gmail.com>
2026-03-13 11:37:40 -0600

    Limpieza y optimizacion del codigo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap()],
-    output: 'static' // <--- Añade esta línea
+    site: 'https://peterolvera.github.io',
+    output: 'static'
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mi CV Desarrollador';
-export const SITE_DESCRIPTION = 'Portafolio y currículum profesional de desarrollador web';
+export const SITE_TITLE = 'Pedro Olvera - Portafolio';
+export const SITE_DESCRIPTION = 'Portafolio profesional de Pedro Alberto Olvera Ochoa - Desarrollador DevOps';
